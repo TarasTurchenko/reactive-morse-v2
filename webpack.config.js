1b68d6211e6a855629922ce4735f2a47019504ea
TarasTurchenko <taras.turchenko@masterofcode.com>
2018-11-18 22:37:39 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    target: 'web',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        host: '0.0.0.0',
+        publicPath: '/dist/',
+        contentBase: path.resolve(__dirname, './src/views/'),
+        watchContentBase: true,
+        compress: true,
+        port: 9001
+    }
+};
